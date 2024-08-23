d20a93910d3d1903ecf1bc56311d3705c5a2da39
Tee56 <sultanbalogun32@gmail.com>
2024-08-23 22:11:20 +0100

    third commit

--- slider.js
+++ slider.js
@@ -1,15 +1,31 @@
-let currentSlide = 0;
+let slideIndex = 1;
+showSlides(slideIndex);
 
-function changeSlide(direction) {
-    const slides = document.querySelector('.slides');
-    const totalSlides = slides.children.length;
-    currentSlide += direction;
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
 
-    if (currentSlide >= totalSlides) {
-        currentSlide = 0; 
-    } else if (currentSlide < 0) {
-        currentSlide = totalSlides - 1; 
+function showSlides(n) {
+    let i;
+
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) {
+        slideIndex = 1
+    }    
+    if (n < 1) {
+        slideIndex = slides.length
     }
-    const offset = -currentSlide * 100;
-    slides.style.transform = `translateX(${offset}%)`;
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
 }
